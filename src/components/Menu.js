65091ddcdfe019fd362a184859489c75656dacd2
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-06 17:39:48 -0600

    Remove useless constructors

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom';
 
 
 export default class Menu extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="navbar-fixed">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Nav extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="navbar-fixed">

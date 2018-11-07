916db1a082b2bdb6e48019a334541773dc28a8f3
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-07 17:55:28 -0600

    Create axios requests

---
+++ src/requests.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const host = "localhost:3000";
+
+export const getUser = (id) => {
+  return axios.get(host + '/users/' + id).then((response) => {
+    return response;
+  }).catch((error) => {
+    return null;
+  });
+};
+
+export const addUser = (username, firstname, lastname, email, phone, password) => {
+  return axios.post(host + '/users', {
+    username: username,
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    phone: phone,
+    password: password
+  }).then((response) => {
+    return true;
+  }).catch((error) => {
+    return false;
+  });
+};
+
+export const updateUser = (id, username, firstname, lastname, email, phone, password) => {
+  return axios.put(host + '/users/' + id, {
+    username: username,
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    phone: phone,
+    password: password
+  }).then((response) => {
+    return true;
+  }).catch((error) => {
+    return false;
+  });
+};
+
+export const getEvents = (id) => {
+  return axios.get(host + '/users/' + id + '/events').then((response) => {
+    return response;
+  }).catch((error) => {
+    return [];
+  });
+};
+
+export const addEvent = (id, dueDate, duration, static) => {
+  return axios.post(host + '/users/' + id + '/events', {
+    duration: duration,
+    dueDate: dueDate,
+    static: static
+  }).then((response) => {
+    return true;
+  }).catch((error) => {
+    return false;
+  });
+};

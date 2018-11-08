b8626e1c0a971a4e8aa953f7d43a5dd0456d1225
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-07 18:07:01 -0600

    Add create button

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 export default class Calendar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      openModal: false
+    };
   }
 
   render() {
     return (
-      <p>Calendar</p>
+      <div>
+        <p>Calendar</p>
+        <div className="fixed-action-btn">
+          <button className="btn-floating btn-large red" onClick={() => {this.setState({openModal: !this.state.openModal});}}>
+            <i className="large material-icons">add</i>
+          </button>
+        </div>
+      </div>
     );
   }
 }

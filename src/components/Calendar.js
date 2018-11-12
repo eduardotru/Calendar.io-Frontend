581efb10e2a7413dc22a6a651600ba6c9889c028
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-11 20:13:31 -0600

    Add friend view of calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -45,48 +45,78 @@ export default class Calendar extends Component {
         dueDate={event.dueDate}
         duration={event.duration}
         onDelete={this.deleteEvent}
+        friendView={this.props.friendView}
       />
     );
-    return (
-      <div>
-        <h3>Calendar</h3>
-        {events}
-        <div className="fixed-action-btn">
-          <Link to="./newEvent" className="btn-floating btn-large deep-purple lighten-3">
-            <i className="large material-icons">add</i>
-          </Link>
+    if(this.props.friendView) {
+      return (
+        <div>
+          <h3>Calendar</h3>
+          {events}
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <h3>Calendar</h3>
+          {events}
+          <div className="fixed-action-btn">
+            <Link to="./newEvent" className="btn-floating btn-large deep-purple lighten-3">
+              <i className="large material-icons">add</i>
+            </Link>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
 class Event extends Component {
   render() {
-    return (
-      <div className="card hoverable grey lighten-3 col s12 m6 l4">
-        <div className="card-title col s12">
-          <p>{this.props.name}</p>
-        </div>
-        <div className="card-content col s12 m6">
-          <div className="row">
-            <div className="col s12 m6">
-              <p>{this.props.dueDate}<i className="material-icons left">calendar_today</i></p>
-            </div>
-            <div className="col s12 m6">
-              <p>{this.props.duration} hours<i className="material-icons left">timelapse</i></p>
+    if(this.props.friendView) {
+      return (
+        <div className="card hoverable grey lighten-3 col s12 m6 l4">
+          <div className="card-title col s12">
+            <p>{this.props.name}</p>
+          </div>
+          <div className="card-content col s12 m6">
+            <div className="row">
+              <div className="col s12 m6">
+                <p>{this.props.dueDate}<i className="material-icons left">calendar_today</i></p>
+              </div>
+              <div className="col s12 m6">
+                <p>{this.props.duration} hours<i className="material-icons left">timelapse</i></p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="card-action">
-          <button
-            className="btn-flat deep-purple-text"
-            onClick={(e) => {this.props.onDelete(this.props.id);}}
-          >
-            Complete event<i className="material-icons left">done</i>
-          </button>
+      );
+    } else {
+      return (
+        <div className="card hoverable grey lighten-3 col s12 m6 l4">
+          <div className="card-title col s12">
+            <p>{this.props.name}</p>
+          </div>
+          <div className="card-content col s12 m6">
+            <div className="row">
+              <div className="col s12 m6">
+                <p>{this.props.dueDate}<i className="material-icons left">calendar_today</i></p>
+              </div>
+              <div className="col s12 m6">
+                <p>{this.props.duration} hours<i className="material-icons left">timelapse</i></p>
+              </div>
+            </div>
+          </div>
+          <div className="card-action">
+            <button
+              className="btn-flat deep-purple-text"
+              onClick={(e) => {this.props.onDelete(this.props.id);}}
+            >
+              Complete event<i className="material-icons left">done</i>
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }

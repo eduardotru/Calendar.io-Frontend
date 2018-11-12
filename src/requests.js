d055a91303e71681f12035cfd2575741766dc150
Eduardo Trujillo <edytr1997@gmail.com>
2018-11-11 23:37:33 -0600

    Change api url

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const host = "http://localhost:3000";
+const host = "http://calendar-ioapi.herokuapp.com";
 
 export const attemptLogin = (username, password) => {
   return axios.get(host + '/login', {
